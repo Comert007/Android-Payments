b169bdb678083c7b8fc12677e8956c42a689777b
sourfeng <sourfeng@163.com>
2017-10-09 10:38:36 +0800

    modify the pay of qq wallet

--- app/src/main/java/com/yw/android/qqwalletpay/qqwallet/QQWalletPay.java
+++ app/src/main/java/com/yw/android/qqwalletpay/qqwallet/QQWalletPay.java
@@ -1,9 +1,12 @@
 package com.yw.android.qqwalletpay.qqwallet;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.widget.Toast;
 
 import com.tencent.mobileqq.openpay.api.IOpenApi;
 import com.tencent.mobileqq.openpay.api.OpenApiFactory;
+import com.tencent.mobileqq.openpay.constants.OpenConstants;
 import com.tencent.mobileqq.openpay.data.pay.PayApi;
 
 /**
@@ -14,18 +17,18 @@ import com.tencent.mobileqq.openpay.data.pay.PayApi;
 public class QQWalletPay {
     private static final String TAG = QQWalletPay.class.getSimpleName();
     /**
-     *  PayApi api = new PayApi();
-     api.appId = APP_ID; // 在http://open.qq.com注册的AppId,参与支付签名，签名关键字key为appId
-     api.serialNumber = ...; // 支付序号,用于标识此次支付
-     api.callbackScheme = ...; // QQ钱包支付结果回调给urlscheme为callbackScheme的activity.，参看后续的“支付回调结果处理”
-     api.tokenId = ...; // QQ钱包支付生成的token_id
-     api.pubAcc = ...; // 手Q公众帐号id.参与支付签名，签名关键字key为pubAcc
-     api.pubAccHint = ...; // 支付完成页面，展示给用户的提示语：提醒关注公众帐号
-     api.nonce = ...; // 随机字段串，每次支付时都要不一样.参与支付签名，签名关键字key为nonce
-     api.timeStamp = ...; // 时间戳，为1970年1月1日00:00到请求发起时间的秒数
-     api.bargainorId = ...; // 商户号.参与支付签名，签名关键字key为bargainorId
-     api.sig = ...; // 商户Server下发的数字签名，生成的签名串，参看“数字签名”
-     api.sigType = "HMAC-SHA1"; // 签名时，使用的加密方式，默认为"HMAC-SHA1"
+     * PayApi api = new PayApi();
+     * api.appId = APP_ID; // 在http://open.qq.com注册的AppId,参与支付签名，签名关键字key为appId
+     * api.serialNumber = ...; // 支付序号,用于标识此次支付
+     * api.callbackScheme = ...; // QQ钱包支付结果回调给urlscheme为callbackScheme的activity.，参看后续的“支付回调结果处理”
+     * api.tokenId = ...; // QQ钱包支付生成的token_id
+     * api.pubAcc = ...; // 手Q公众帐号id.参与支付签名，签名关键字key为pubAcc
+     * api.pubAccHint = ...; // 支付完成页面，展示给用户的提示语：提醒关注公众帐号
+     * api.nonce = ...; // 随机字段串，每次支付时都要不一样.参与支付签名，签名关键字key为nonce
+     * api.timeStamp = ...; // 时间戳，为1970年1月1日00:00到请求发起时间的秒数
+     * api.bargainorId = ...; // 商户号.参与支付签名，签名关键字key为bargainorId
+     * api.sig = ...; // 商户Server下发的数字签名，生成的签名串，参看“数字签名”
+     * api.sigType = "HMAC-SHA1"; // 签名时，使用的加密方式，默认为"HMAC-SHA1"
      */
 
     private Context context;
@@ -39,16 +42,57 @@ public class QQWalletPay {
     private String timeStamp;
     private String bargainorId;
     private String sig;
-    private String sigType="HMAC-SHA1";
+    private String sigType = "HMAC-SHA1";
 
     private IOpenApi openApi;
 
-    public void requestPay(){
-        openApi = OpenApiFactory.getInstance(context,null);
+    public QQWalletPay() {
+    }
+
+    public void walletPay() {
+
+        openApi = OpenApiFactory.getInstance(context, this.appId);
+
+        boolean installed = openApi.isMobileQQInstalled();
+        boolean supported = openApi.isMobileQQSupportApi(OpenConstants.API_NAME_PAY);
+
+        if (!installed) {
+            showToast("请先安装最新版的手机QQ");
+            return;
+        }
+
+        if (!supported) {
+            showToast("当前版本不支持手机qq钱包支付");
+            return;
+        }
+
+        PayApi pay = new PayApi ();
+        pay.appId = this.appId;
+        pay.serialNumber = this.serialNumber;
+        pay.callbackScheme = this.callbackScheme;
+        pay.tokenId = this.tokenId;
+        pay.pubAcc = this.pubAcc;
+        pay.pubAccHint = this.pubAccHint;
+        pay.nonce = this.nonce;
+        pay.timeStamp = Long.parseLong(this.timeStamp);
+        pay.bargainorId = this.bargainorId;
+        pay.sig = this.sig;
+        pay.sigType = this.sigType;
+
+        if (pay.checkParams()){
+            openApi.execApi(pay);
+        }else {
+            showToast("支付参数不完整");
+        }
     }
 
 
-    public static class Builder{
+    private void showToast(String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
+
+    public static class Builder {
 
         private Context context;
         private String appId;
@@ -61,7 +105,7 @@ public class QQWalletPay {
         private String timeStamp;
         private String bargainorId;
         private String sig;
-        private String sigType="HMAC-SHA1";
+        private String sigType = "HMAC-SHA1";
 
         public Builder setContext(Context context) {
             this.context = context;
@@ -124,21 +168,26 @@ public class QQWalletPay {
         }
 
 
-        public PayApi create(){
-            PayApi api = new PayApi();
-            api.serialNumber = this.serialNumber;
-            api.callbackScheme = this.callbackScheme;
-            api.tokenId = this.tokenId;
-            api.pubAcc = this.pubAcc;
-            api.pubAccHint = this.pubAccHint;
-            api.nonce = this.nonce;
-            api.timeStamp = Long.parseLong(this.timeStamp);
-            api.bargainorId = this.bargainorId;
-            api.sig = this.sig;
-            api.appId = this.appId;
-            api.sigType = this.sigType;
+        public QQWalletPay create() {
+            QQWalletPay pay = new QQWalletPay();
+            pay.context = this.context;
+            pay.serialNumber = this.serialNumber;
+            pay.callbackScheme = this.callbackScheme;
+            pay.tokenId = this.tokenId;
+            pay.pubAcc = this.pubAcc;
+            pay.pubAccHint = this.pubAccHint;
+            pay.nonce = this.nonce;
+            pay.timeStamp = this.timeStamp;
+            pay.bargainorId = this.bargainorId;
+            pay.sig = this.sig;
+            pay.appId = this.appId;
+            if (!TextUtils.isEmpty(this.sigType)) {
+                pay.sigType = this.sigType;
+            }else {
+                pay.sigType = "HMAC-SHA1";
+            }
 
-            return api;
+            return pay;
         }
     }
 }
---
+++ app/src/main/java/com/yw/android/qqwalletpay/qqwallet/WeChatCallbackActivity.java
@@ -0,0 +1,58 @@
+package com.yw.android.qqwalletpay.qqwallet;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tencent.mobileqq.openpay.api.IOpenApi;
+import com.tencent.mobileqq.openpay.api.IOpenApiListener;
+import com.tencent.mobileqq.openpay.api.OpenApiFactory;
+import com.tencent.mobileqq.openpay.data.base.BaseResponse;
+import com.tencent.mobileqq.openpay.data.pay.PayResponse;
+import com.yw.android.qqwalletpay.R;
+
+/**
+ * Created by feng on 2017/10/9.
+ */
+
+public class WeChatCallbackActivity extends AppCompatActivity implements IOpenApiListener{
+
+    private IOpenApi openApi;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_callback);
+
+        openApi = OpenApiFactory.getInstance(this, "app id");
+        openApi.handleIntent(getIntent(), this);
+    }
+
+    @Override
+    public void onOpenResponse(BaseResponse response) {
+        String message;
+        if (response == null) {
+            // 不能识别的intent
+            return;
+        } else {
+            if (response instanceof PayResponse) {
+                // 支付回调响应
+                PayResponse payResponse = (PayResponse) response;
+
+                message = " apiName:" + payResponse.apiName
+                        + " serialnumber:" + payResponse.serialNumber
+                        + " isSucess:" + payResponse.isSuccess()
+                        + " retCode:" + payResponse.retCode
+                        + " retMsg:" + payResponse.retMsg
+                        + " transactionId:" + payResponse.transactionId;
+                if (payResponse.isSuccess()) {
+                    // 支付成功，这个支付结果不能作为发货的依据
+                }
+            } else {
+                // 不能识别的响应
+            }
+        }
+    }
+
+
+}
